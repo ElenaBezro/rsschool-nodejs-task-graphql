f4ddac4b0d82b8170a039cd19ece04860b4d1bcb
elenaBezro <lenyka_86@mail.ru>
2023-01-25 20:56:34 +0100

    feat: add user endpoints

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,78 +1,103 @@
-import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { idParamSchema } from '../../utils/reusedSchemas';
-import {
-  createUserBodySchema,
-  changeUserBodySchema,
-  subscribeBodySchema,
-} from './schemas';
-import type { UserEntity } from '../../utils/DB/entities/DBUsers';
+import { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
+import { idParamSchema } from "../../utils/reusedSchemas";
+import { createUserBodySchema, changeUserBodySchema, subscribeBodySchema } from "./schemas";
+import type { UserEntity } from "../../utils/DB/entities/DBUsers";
 
-const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
-  fastify
-): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<UserEntity[]> {});
+const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> => {
+  fastify.get("/", async function (request, reply): Promise<UserEntity[]> {
+    return await fastify.db.users.findMany();
+  });
 
   fastify.get(
-    '/:id',
+    "/:id",
     {
       schema: {
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<UserEntity> {}
+    async function (request, reply): Promise<UserEntity | null> {
+      const user = await fastify.db.users.findOne({ key: "id", equals: request.params.id });
+      return user;
+    }
   );
 
   fastify.post(
-    '/',
+    "/",
     {
       schema: {
         body: createUserBodySchema,
       },
     },
-    async function (request, reply): Promise<UserEntity> {}
+    async function (request, reply): Promise<UserEntity> {
+      return await fastify.db.users.create(request.body);
+    }
   );
 
   fastify.delete(
-    '/:id',
+    "/:id",
     {
       schema: {
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<UserEntity> {}
+    async function (request, reply): Promise<UserEntity> {
+      return await fastify.db.users.delete(request.id);
+    }
   );
 
   fastify.post(
-    '/:id/subscribeTo',
+    "/:id/subscribeTo",
     {
       schema: {
         body: subscribeBodySchema,
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<UserEntity> {}
+    async function (request, reply): Promise<UserEntity> {
+      const { userId } = request.body;
+      let user = await fastify.db.users.findOne(request.id);
+      if (user && !user.subscribedToUserIds.includes(userId)) {
+        user = await fastify.db.users.change(request.id, {
+          subscribedToUserIds: [...user.subscribedToUserIds, userId],
+        });
+      }
+
+      return user!;
+    }
   );
 
   fastify.post(
-    '/:id/unsubscribeFrom',
+    "/:id/unsubscribeFrom",
     {
       schema: {
         body: subscribeBodySchema,
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<UserEntity> {}
+    async function (request, reply): Promise<UserEntity> {
+      const { userId } = request.body;
+      let user = await fastify.db.users.findOne(request.id);
+      if (user && user.subscribedToUserIds.includes(userId)) {
+        user = await fastify.db.users.change(request.id, { subscribedToUserIds: [...user.subscribedToUserIds.filter((userIdToFilter) => userIdToFilter !== userId)] });
+      }
+      return user!;
+
+      //return await
+    }
   );
 
   fastify.patch(
-    '/:id',
+    "/:id",
     {
       schema: {
         body: changeUserBodySchema,
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<UserEntity> {}
+    async function (request, reply): Promise<UserEntity> {
+      const user = await fastify.db.users.change(JSON.stringify({ firstName: request.body.firstName, lastName: request.body.lastName, email: request.body.email }), request.id);
+      return user;
+    }
   );
 };
 
