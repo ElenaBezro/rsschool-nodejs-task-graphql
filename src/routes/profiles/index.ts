c15b9c1d03f755bcf73f131472b4f94527619a02
elenaBezro <lenyka_86@mail.ru>
2023-01-27 13:22:18 +0100

    feat: validate profile request data

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -1,4 +1,6 @@
+import { validate } from "uuid";
 import { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
+
 import { idParamSchema } from "../../utils/reusedSchemas";
 import { createProfileBodySchema, changeProfileBodySchema } from "./schema";
 import type { ProfileEntity } from "../../utils/DB/entities/DBProfiles";
@@ -16,8 +18,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> 
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
+      if (typeof request.params.id !== "string") {
+        reply.statusCode = 400;
+        throw new Error("Invalid id");
+      }
       const profile = await fastify.db.profiles.findOne({ key: "id", equals: request.params.id });
-      return profile!;
+      if (profile) {
+        return profile;
+      }
+
+      reply.statusCode = 404;
+      throw new Error("Not exist");
     }
   );
 
@@ -29,8 +40,35 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> 
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
-      const profile = await fastify.db.profiles.create(request.body);
-      return profile!;
+      const { userId, memberTypeId } = request.body;
+
+      if (!validate(userId)) {
+        reply.statusCode = 400;
+        throw new Error("Invalid user id");
+      }
+      const user = await fastify.db.users.findOne({ key: "id", equals: userId });
+
+      if (!user) {
+        reply.statusCode = 400;
+        throw new Error(`User with userId = ${userId} does not exist`);
+      }
+
+      const profile = await fastify.db.profiles.findOne({ key: "userId", equals: userId });
+
+      if (profile) {
+        reply.statusCode = 400;
+        throw new Error("User already has a profile");
+      }
+
+      const memberType = await fastify.db.memberTypes.findOne({ key: "id", equals: memberTypeId });
+
+      if (!memberType) {
+        reply.statusCode = 400;
+        throw new Error(`Member type = ${memberTypeId} does not exist`);
+      }
+
+      const newProfile = await fastify.db.profiles.create(request.body);
+      return newProfile;
     }
   );
 
@@ -42,7 +80,13 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> 
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
-      return await fastify.db.profiles.delete(request.id);
+      const profile = await fastify.db.profiles.findOne({ key: "id", equals: request.params.id });
+      if (!profile) {
+        reply.statusCode = 400;
+        throw new Error("Not exist");
+      }
+
+      return await fastify.db.profiles.delete(request.params.id);
     }
   );
 
@@ -55,16 +99,16 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> 
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
+      const profile = await fastify.db.profiles.findOne({ key: "id", equals: request.params.id });
+      if (!profile) {
+        reply.statusCode = 400;
+        throw new Error("Not exist");
+      }
+
       const { avatar, sex, birthday, country, street, city, memberTypeId } = request.body;
-      return await fastify.db.profiles.change(request.id, {
-        avatar,
-        sex,
-        birthday,
-        country,
-        street,
-        city,
-        memberTypeId,
-      });
+      // const changes = Object.entries({ avatar, sex, birthday, country, street, city, memberTypeId }).reduce((acc, [key, value]) => (value !== undefined ? { ...acc, [key]: value } : acc), {});
+      // return await fastify.db.profiles.change(request.params.id, changes);
+      return await fastify.db.profiles.change(request.params.id, { avatar, sex, birthday, country, street, city, memberTypeId });
     }
   );
 };
