18743a87e0558cb4e131de9b3ed3c524179d7612
elenaBezro <lenyka_86@mail.ru>
2023-01-27 20:54:15 +0100

    feat: add qraphql types

--- src/routes/graphql/schema.ts
+++ src/routes/graphql/schema.ts
@@ -1,34 +1,131 @@
+import fastify from "fastify";
+import { GraphQLList, GraphQLObjectType } from "graphql";
+import { GraphQLInt, GraphQLSchema, GraphQLString } from "graphql/type";
+import { changeMemberTypeBodySchema } from "../member-types/schema";
+
 export const graphqlBodySchema = {
-  type: 'object',
+  type: "object",
   properties: {
-    mutation: { type: 'string' },
-    query: { type: 'string' },
+    mutation: { type: "string" },
+    query: { type: "string" },
     variables: {
-      type: 'object',
+      type: "object",
     },
   },
   oneOf: [
     {
-      type: 'object',
-      required: ['query'],
+      type: "object",
+      required: ["query"],
       properties: {
-        query: { type: 'string' },
+        query: { type: "string" },
         variables: {
-          type: 'object',
+          type: "object",
         },
       },
       additionalProperties: false,
     },
     {
-      type: 'object',
-      required: ['mutation'],
+      type: "object",
+      required: ["mutation"],
       properties: {
-        mutation: { type: 'string' },
+        mutation: { type: "string" },
         variables: {
-          type: 'object',
+          type: "object",
         },
       },
       additionalProperties: false,
     },
   ],
 } as const;
+
+// const memberTypesData = [
+//   { id: "basic", discount: 10, monthPostsLimit: 50 },
+//   { id: "business", discount: 20, monthPostsLimit: 100 },
+// ];
+
+export const memberTypeType = new GraphQLObjectType({
+  name: "memberType",
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    discount: {
+      type: GraphQLInt,
+    },
+    monthPostsLimit: {
+      type: GraphQLInt,
+    },
+  }),
+});
+
+export const postType = new GraphQLObjectType({
+  name: "post",
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    title: {
+      type: GraphQLString,
+    },
+    content: {
+      type: GraphQLString,
+    },
+    userId: {
+      type: GraphQLString,
+    },
+  }),
+});
+
+export const profileType = new GraphQLObjectType({
+  name: "profile",
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    avatar: {
+      type: GraphQLString,
+    },
+    sex: {
+      type: GraphQLString,
+    },
+    birthday: {
+      type: GraphQLString,
+    },
+    country: {
+      type: GraphQLString,
+    },
+    street: {
+      type: GraphQLString,
+    },
+    city: {
+      type: GraphQLString,
+    },
+    memberTypeId: {
+      type: GraphQLString,
+    },
+    userId: {
+      type: GraphQLString,
+    },
+  }),
+});
+
+export const userType = new GraphQLObjectType({
+  name: "user",
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    firstName: {
+      type: GraphQLString,
+    },
+    lastName: {
+      type: GraphQLString,
+    },
+    email: {
+      type: GraphQLString,
+    },
+    subscribedToUserIds: {
+      type: new GraphQLList(GraphQLString),
+    },
+  }),
+});
